5f67115e80c88759aee6d8d1d805e1d29a7e7339
apolisskyi <apolisskyi@n-ix.com>
2022-12-09 20:54:01 +0200

    init new MPLAB-X project

---
+++ firmware/open-risk-logger-samd21-xpro-proto.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=open-risk-logger-samd21-xpro-proto.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/open-risk-logger-samd21-xpro-proto.X.production.hex
